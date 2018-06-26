2bf0d81ba4951a253a015f5de0c2765aacd75c88
Oleg Filonchuk <oifilonchuk@gmail.com>
2018-06-26 23:54:08 +0300

    added responsibility to main page

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('.sliderwrap').slick({
+        responsive: {
+            breakpoint: 1000,
+            settings: {
+                arrows: false
+            }
+        },
         autoplay: true,
         dots: true,
         dotsClass: 'dots-container',
