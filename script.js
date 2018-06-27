aed48af18102f000472a9f2649e18f9bd475b19a
unknown <larahka777@gmail.com>
2018-06-27 08:17:57 +0300

    LOL minus noch

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 $(document).ready(function(){
     // $('.dropdown-toggle').dropdown();
     $('.sliderwrap').slick({
-        responsive: {
-            breakpoint: 1000,
-            settings: {
-                arrows: false
-            }
-        },
-        autoplay: true,
+		autoplay: true,
         dots: true,
         dotsClass: 'dots-container',
         prevArrow: $('#prev'),
-        nextArrow: $('#next')
+        nextArrow: $('#next'),
+		swipe: true,
+        responsive: [
+		{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: false,
+            }
+        },
+		{
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: false,
+            }
+        }
+		]
     });
 });
